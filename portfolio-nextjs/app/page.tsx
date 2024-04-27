90c2fa6507966a51528a5a870cda5c9c797e4b9e
Erick Pinedo <124737187+Erickferpinedo@users.noreply.github.com>
2024-04-27 12:25:12 -0400

    d

--- portfolio-nextjs/app/page.tsx
+++ portfolio-nextjs/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="w-full md:w-2/5 lg:w-2/5 mb-4 md:mb-0 lg:mb:0 flex justify-center">
           <h2 className="text-4xl font-extrabold dark:text-white">Erick Pinedo</h2>
         </div>
-        <div className="w-full md:w-2/5 flex justify-between">
+        <div className="w-full md:w-2/5 flex justify-between ">
           <a href="#about">About</a>
           <a href="#Experience">Experience</a>
           <a href="#Projects">Projects</a>
@@ -43,7 +43,7 @@ export default function Home() {
           {/* <h2 className="mb-4 mt-4"><span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Full Stack Developer</span></h2> */}
           <div className="mb-4 mt-4 flex flex-row">
             <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Downloadd CV</button>
-            <button id="Contact" type="button"  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" >Contact Info</button> <a href="#Contact"></a>
+            <button type="button"  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" >Contact Info</button> <a></a>
             
 
           </div>
@@ -72,6 +72,15 @@ export default function Home() {
               height={30}
               alt="Picture of the author"
               className="ml-2 mr-2" />
+              </a>
+              <a href="https://wa.me/17862006991">
+              <Image
+                src="/whatsaapimage.jpg"
+                width={30}
+                height={30}
+                alt="Picture of the author"
+                className="ml-2 mr-2"
+            />
               </a>
           </div>
 
@@ -412,19 +421,19 @@ export default function Home() {
 
 </div>
 
-<div id="Contact" > 
+<div> 
 <div className="flex flex-col md:flex-row lg:flex-row justify-center items-center ">
 <p className="mb-5 mt-5">          <small className="font-medium text-lg text-gray-500 dark:text-gray-400"></small>Get In Touch</p>
 </div>
 <div className="flex flex-col md:flex-row lg:flex-row justify-center items-center">
-<h1 className="text-4xl font-bold dark:text-white mb-3">Contact Me</h1>
+<h1 className="text-4xl font-bold dark:text-white mb-3" id="Contact">Contact Me</h1>
 </div>
 </div>
 
 
 <div className="details-container flex-1 bg-white p-4 md:p-6 pl-4 md:pl-6 rounded-2xl border border-gray-300 text-center mt-4 md:mt-2 md:mr-2 justify-between">
     <div className="mb-4 mt-8 md:mt-0 flex flex-col md:flex-row lg:flex-row justify-between">
-        <div className="flex items-center mb-2 md:mb-0 dark:text-black mt-5">
+        <div className="flex items-center mb-2 md:mb-0 dark:text-black">
             <Image
                 src="/email.png"
                 width={30}
